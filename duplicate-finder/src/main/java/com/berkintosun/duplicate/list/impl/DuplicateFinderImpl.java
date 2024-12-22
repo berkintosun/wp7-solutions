2fe6d0ced4af433d05e85b301bc76cb2b20d1665
Berkin Tosun <berkintsn@gmail.com>
2024-12-23 00:54:20 +0200

    Implementing the first iteration of the solution

--- duplicate-finder/src/main/java/com/berkintosun/duplicate/list/impl/DuplicateFinderImpl.java
+++ duplicate-finder/src/main/java/com/berkintosun/duplicate/list/impl/DuplicateFinderImpl.java
@@ -6,6 +6,29 @@ import java.util.*;
 
 public class DuplicateFinderImpl<T> implements DuplicateFinder<List<T>> {
     public List<T> findDuplicates(List<T> inputList) {
-        return new ArrayList<T>();
+        if (inputList == null) {
+            throw new IllegalArgumentException();
+        }
+
+        Map<T, Integer> countMap = new HashMap<>();
+        Set<T> added = new HashSet<>();
+        List<T> result = new ArrayList<>();
+
+        for (T item : inputList) {
+            if (countMap.containsKey(item)) {
+                countMap.put(item, 1 + countMap.get(item));
+            } else {
+                countMap.put(item, 1);
+            }
+        }
+
+        for (T item : inputList) {
+            if (countMap.get(item) > 1 && !added.contains(item)) {
+                result.add(item);
+                added.add(item);
+            }
+        }
+
+        return result;
     }
 }
